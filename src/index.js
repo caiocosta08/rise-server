6ddeb430a87620c8eec2f04c11378887a9bd37ea
Caio <caiocostaee@gmail.com>
2019-10-01 14:52:12 -0300

    server config running

---
+++ src/index.js
@@ -0,0 +1,19 @@
+const dotenv = require('dotenv');
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+dotenv.config();
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+app.listen( process.env.PORT || 8000, () => {
+  const message = 'Server running on port 8000';
+  console.log(message);
+});
