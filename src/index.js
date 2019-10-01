9968a3f47665e6879f0a0b3aee2e72bbd8a182dd
Caio <caiocostaee@gmail.com>
2019-10-01 16:18:15 -0300

    formating with prettier

--- src/index.js
+++ src/index.js
@@ -1,21 +1,21 @@
-const dotenv = require('dotenv');
-const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
+const dotenv = require("dotenv");
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
 
 dotenv.config();
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
-require('./controllers/users.controller')(app);
+require("./controllers/users.controller")(app);
 
-app.get('/', (req, res) => {
-  res.send('Hello World');
+app.get("/", (req, res) => {
+    res.send("Hello World");
 });
 
-app.listen( process.env.PORT || 8000, () => {
-  const message = 'Server running on port 8000';
-  console.log(message);
+app.listen(process.env.PORT || 8000, () => {
+    const message = "Server running on port 8000";
+    console.log(message);
 });
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,43 +1,43 @@
-const mongoose = require('../database');
-const bcrypt = require('bcryptjs');
+const mongoose = require("../database");
+const bcrypt = require("bcryptjs");
 
 const UserSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    lowercase: true,
-  },
-  password: {
-    type: String,
-    required: true,
-    select: false,
-  },
-  accountType: {
-    type: String,
-    required: true,
-    default: "user"
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-  updatedAt: {
-    type: Date,
-    default: Date.now,
-  }
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true
+    },
+    password: {
+        type: String,
+        required: true,
+        select: false
+    },
+    accountType: {
+        type: String,
+        required: true,
+        default: "user"
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    updatedAt: {
+        type: Date,
+        default: Date.now
+    }
 });
 
-UserSchema.pre('save', async function(next){
-  const hash = await bcrypt.hash(this.password, 10);
-  this.password = hash;
-  next();
+UserSchema.pre("save", async function(next) {
+    const hash = await bcrypt.hash(this.password, 10);
+    this.password = hash;
+    next();
 });
 
-const User = mongoose.model('User', UserSchema);
+const User = mongoose.model("User", UserSchema);
 
 module.exports = User;
